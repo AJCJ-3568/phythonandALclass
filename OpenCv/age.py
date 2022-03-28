bda0c593c3a3410a06f40d7454d5f6465b4fb1d6
AJCJ-3568 <84306031+AJCJ-3568@users.noreply.github.com>
2022-03-28 22:25:50 +0530

    class to 2 files add to deploy  and age net  res10

--- OpenCv/age.py
+++ OpenCv/age.py
@@ -2,3 +2,58 @@ import cv2
 import os
 import filetype
 import numpy as np
+
+
+# The model architecture
+# download from: https://drive.google.com/open?id=1kiusFljZc9QfcIYdU2s7xrtWHTraHwmW
+AGE_MODEL = 'deploy_age.prototxt'
+# The model pre-trained 
+# download from: https://drive.google.com/open?id=1kWv0AjxGSN0g31OeJa02eBGM0R_jcjIl
+AGE_PROTO = 'age_net.caffemodel'
+# Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
+# substraction to eliminate the effect of illunination changes
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+# Represent the 8 age classes of this CNN probability layer
+AGE_INTERVALS = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)',
+                 '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+# download from: https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+FACE_PROTO = "deploy.prototxt.txt"
+# download from: https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
+FACE_MODEL = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
+# Initialize frame size
+frame_width = 1280
+frame_height = 720
+# load face Caffe model
+face_net = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
+# Load age prediction model
+age_net = cv2.dnn.readNetFromCaffe(AGE_MODEL, AGE_PROTO)
+
+#get faces method
+
+def get_faces(frame, confidence_threshold=0.5):
+    """Returns the box coordinates of all detected faces"""
+    # convert the frame into a blob to be ready for NN input
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 177.0, 123.0))
+    # set the image as input to the NN
+    face_net.setInput(blob)
+    # perform inference and get predictions
+    output = np.squeeze(face_net.forward())
+    # initialize the result list
+    faces = []
+    # Loop over the faces detected
+    for i in range(output.shape[0]):
+        confidence = output[i, 2]
+        if confidence > confidence_threshold:
+            box = output[i, 3:7] * np.array([frame_width, frame_height, frame_width, frame_height])
+            # convert to integers
+            start_x, start_y, end_x, end_y = box.astype(np.int)
+            # widen the box a little
+            start_x, start_y, end_x, end_y = start_x - \
+                10, start_y - 10, end_x + 10, end_y + 10
+            start_x = 0 if start_x < 0 else start_x
+            start_y = 0 if start_y < 0 else start_y
+            end_x = 0 if end_x < 0 else end_x
+            end_y = 0 if end_y < 0 else end_y
+            # append to our list
+            faces.append((start_x, start_y, end_x, end_y))
+    return faces
